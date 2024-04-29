c8af29daf75f1a65e81c23e90200aadf3339fc6e
Anastasiya Omak <ios.developer.by@gmail.com>
2024-04-29 23:06:53 +0300

    Закончена работа над Alert, ActionSheet

---
+++ AlertActionSheet/AlertActionSheetApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlertActionSheetApp.swift
+//  AlertActionSheet
+//
+//  Created by Anastasiya Omak on 29/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct AlertActionSheetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
